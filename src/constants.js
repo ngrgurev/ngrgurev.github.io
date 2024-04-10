1c230a90c273046593251467444dd334977b6b55
Niko <127935571+ngrgurev@users.noreply.github.com>
2024-04-10 14:13:36 +0200

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `So you've found my desk and my laptop! This is where I do my work. I'm currently employed as a data analyst for Wolt in South - East Europe region. I usually use Python, Looker and Excel/Google Sheets for my day-to-day tasks.
+  Here is my <a href="https://github.com/ngrgurev" target="_blank">Github account</a>! In my free time, I like to do sports analysis and research blockchain and web3 topics.`,
+  "cs-degree": `This is my Data Science NEET Academy degree. I framed it because it represents the hard work I did to get where I am right now. It gave me a solid foundation of data science, and I love it.`,
+  "sofa-table": `That's my couch. I like to kick back and relax here, mostly playing Hearthstone Battlegrounds and FPL. Here's my team ID, if you ever wanna see the worst possible picks for upcoming gameweeks (jk, I'm actually doing quite good): 5857185`,
+  tv: `That's my TV where I stream Youtube videos and watch TV shows. What do I watch on Youtube, you might ask? Well, here are a couple of channels that I follow:
+  <a href="https://www.youtube.com/@AlexTheAnalyst" target="_blank">AlexTheAnalyst</a>, <a href="https://www.youtube.com/@mo-chen" target="_blank">Mo Chen</a>,
+  <a href="https://www.youtube.com/@hubermanlab" target="_blank">Andrew Huberman</a>, <a href="https://www.youtube.com/@robmulla" target="_blank">Rob Mulla</a>, <a href="https://www.youtube.com/@KenJee_ds" target="_blank">Ken Jee</a>, and <a href="https://www.youtube.com/@dogdogtwitch" target="_blank">dogdog</a> !`,
+  bed: `Sneaky little man, you've found my bed even! As you can see, my whole appartment is practical, that's my life phylosophy, and inspiration comes from <a href="https://www.youtube.com/@nevertoosmall" target="_blank">NeverTooSmall</a>. There's my journal as well, but I'm not letting you touch that!`,
+  resume: `Oh my, oh my, what do we have here? It's my resume! <a href="https://github.com/ngrgurev/Resume/blob/main/NikoResume.pdf" target="_blank">Might as well check it!</a>
+  If you want to reach out, my mail is ngrgurev@icloud.com ! If you have some interesting job or collab opportunities, feel free to hit me up!`,
+  projects: `I've listed a couple of projects for your interest <a href="https://www.datascienceportfol.io/grgurev" target="_blank">riiight here.</a> `,
+  library: `I take my books very seriously. This is my library with quite a lot of books. My digital library though is <a href="https://www.goodreads.com/user/show/168471237" target="_blank">right here</a>! Feel free to check it out! (Also, I am open for any book recommendations :D)`,
+  exit: `Already going? Sure you don't want to stay for coffee? Well, I don't blame you, nature really is beautiful. Before you leave, let me grab a business card so we can stay in touch. <a href="https://www.linkedin.com/in/grgurev" target="_blank">LinkedIn</a>, Discord: @guerilladata. Okay, just close the tab and I'll see you around!`,
+};
